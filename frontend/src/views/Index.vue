65e7d39d3504a34c00be5fdc9bfef233cd1a0f9a
Muusa1617 <602266870@qq.com>
2022-12-14 17:25:25 +0800

    写入一个Button

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -3,15 +3,13 @@
     <BlogHeader/>
 
     <ArticleList/>
-    <div class="hello">
-      <button type="button" class="btn btn-primary">Primary</button>
-    </div>
 
     <BlogFooter/>
 
 </template>
 
 <script>
+
     import BlogHeader from '@/components/BlogHeader.vue'
     import BlogFooter from '@/components/BlogFooter.vue'
     import ArticleList from '@/components/ArticleList.vue'
